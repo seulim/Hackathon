529f559fc694cf8a0908def2fa7b9464ea44d6d9
pjeon <pjeon@ebay.com>
2016-12-06 14:29:26 +0900

    Commit

--- Hackathon2016/Hackathon2016/Controllers/HomeController.cs
+++ Hackathon2016/Hackathon2016/Controllers/HomeController.cs
@@ -9,38 +9,41 @@ using Google.Apis.Vision.v1;
 
 namespace Hackathon2016.Controllers
 {
-    public class HomeController : Controller
-    {
+	public class HomeController : Controller
+	{
 		[HttpPost]
 		public ActionResult ImageDetect(string filepath)
 		{
+			string retText = "";
 			filepath = filepath.Replace("\\", "/");
 
 			TextDetectionBiz biz = new TextDetectionBiz();
 			VisionService client = biz.CreateAuthorizedClient();
 			IList<AnnotateImageResponse> response = biz.DetectText(client, filepath);
-			
 
-			return Json(response[0].TextAnnotations[0].Description);
+			if (response != null && response[0].TextAnnotations != null && response[0].TextAnnotations[0].Description != null)
+				retText = response[0].TextAnnotations[0].Description;
+
+			return Json(retText);
 		}
 
 		public ActionResult Index()
-        {
-            return View();
-        }
+		{
+			return View();
+		}
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+			return View();
+		}
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
-    }
+			return View();
+		}
+	}
 }
